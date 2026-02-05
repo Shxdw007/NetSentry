60e0d3d7375515dcc4f092c46b597b41400f07e1
Danil <71333561+Shxdw007@users.noreply.github.com>
2026-02-05 18:08:31 +0800

    Add Users DbSet to AppDbContext

--- NetSentry.Server/Data/AppDbContext.cs
+++ NetSentry.Server/Data/AppDbContext.cs
@@ -1,33 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using NetSentry.Server.Models;
 
-namespace NetSentry.Server.Data;
-
-public class AppDbContext : DbContext
+namespace NetSentry.Server.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+    public class AppDbContext : DbContext
     {
-    }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Machine> Machines { get; set; }
-    public DbSet<Metric> Metrics { get; set; }
-    public DbSet<Disk> Disks { get; set; }
+        public DbSet<Machine> Machines { get; set; }
+        public DbSet<Metric> Metrics { get; set; }
+        public DbSet<Disk> Disks { get; set; }
+        public DbSet<EventLog> EventLogs { get; set; }
+        public DbSet<User> Users { get; set; } // Добавили таблицу пользователей
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-        // Machine -> Metrics (1:N)
-        modelBuilder.Entity<Metric>()
-            .HasOne(m => m.Machine)
-            .WithMany(x => x.Metrics)
-            .HasForeignKey(m => m.MachineId);
-
-        // Machine -> Disks (1:N)
-        modelBuilder.Entity<Disk>()
-            .HasOne(d => d.Machine)
-            .WithMany(x => x.Disks)
-            .HasForeignKey(d => d.MachineId);
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique(); // Уникальные логины
+        }
     }
 }
